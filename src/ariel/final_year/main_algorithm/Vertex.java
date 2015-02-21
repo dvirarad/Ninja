046a52ef776a306efd6b2117bc190f630f842c63
jeremieG <jeremiegabay@gmail.com>
2015-02-21 18:02:56 +0200

    main algorithm package

---
+++ src/ariel/final_year/main_algorithm/Vertex.java
@@ -0,0 +1,38 @@
+package ariel.final_year.main_algorithm;
+
+public class Vertex {
+
+	private final int RANGE = 10;
+	private final int AFTER_DOT = 100;//3 number after dot 
+	
+	public double x,y,z;
+
+	public Vertex(){
+		x = Math.floor((Math.random()*RANGE)*AFTER_DOT)/AFTER_DOT;
+		y = Math.floor((Math.random()*RANGE)*AFTER_DOT)/AFTER_DOT;
+		z = Math.floor((Math.random()*RANGE)*AFTER_DOT)/AFTER_DOT;
+	}
+	
+	public Vertex(double newX, double newY, double newZ) {
+		x = newX;
+		y = newY;
+		z = newZ;
+	}
+	
+	public Vertex(Vertex v) {
+		x = v.x;
+		y = v.y;
+		z = v.z;
+	}
+	
+	public Vertex(int boxSize, int range) {
+		x = Math.floor(Math.random()*boxSize*range)/range;
+		y = Math.floor(Math.random()*boxSize*range)/range;
+		z = Math.floor(Math.random()*boxSize*range)/range;
+	}
+
+	@Override
+	public String toString() {
+		return "("+ x +", " + y + ", " + z + ")";
+	}
+}
